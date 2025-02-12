6a27576f657f2781e16ceb02e0e8178b136a91f6
Kiattisak <kiattisak@ada-soft.com>
2025-02-13 00:04:11 +0700

    Updates

---
+++ precache-manifest.331f96b36501cc3569a5bbf791c9ac59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "492b4d5259998635eb5bb9952a6f6bfb",
+    "url": "/demo-react/index.html"
+  },
+  {
+    "revision": "f2153e20fc4c8699449e",
+    "url": "/demo-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/demo-react/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "f2153e20fc4c8699449e",
+    "url": "/demo-react/static/js/main.abfd7462.chunk.js"
+  },
+  {
+    "revision": "78b1d8585bcdb7c31c97",
+    "url": "/demo-react/static/js/runtime~main.15d0518b.js"
+  }
+]);
